eb200d18a36af1bea029839cdca67e9242f91a01
Renato Araga <admin@bkprecisioninstruments.com>
2018-09-24 11:54:45 -0700

    Intial complete

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Provider } from 'react-redux';
+import Posts from './components/Posts';
+import Postsform from './components/Postsform';
+import store from './store';
+
+class App extends Component {
+  render() {
+    return (
+     <Provider store={store}>
+      <div className="App">
+          <Postsform />
+          <Posts /> 
+        </div>
+     </Provider> 
+    );
+  }
+}
+
+export default App;
